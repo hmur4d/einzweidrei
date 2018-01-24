e00f917f36100f36d38ba3da89382a1a059455e7
Vincent Fiack <vfiack@rs2d.com>
2018-01-24 12:14:11 +0100

    log avec timestamp

--- userspace/log.c
+++ userspace/log.c
@@ -1,13 +1,22 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <time.h>
+
 #include "log.h"
 
-void _log(char* prefix, char* format, va_list args) {
-	printf("[%s] ", prefix);
+void _log(char* level, char* format, va_list args) {
+	//TODO: multithread => locks
+
+	time_t rawtime;
+	time(&rawtime);
+	struct tm *timeinfo = localtime(&rawtime);
+
+	printf("[%s][%02d/%02d/%04d][%02d:%02d:%02d] ", 
+		level, 
+		timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 	vprintf(format, args);
 	printf("\n");
 
-	//TODO: add timestamp
 	//TODO: save to file too
 }
 
