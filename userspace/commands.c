6f3fc0734cbb71a2d03662ab179b84e9066e99d9
Vincent Fiack <vfiack@rs2d.com>
2018-01-26 14:20:01 +0100

    implémentation messages nécessaires pour que la connexion avec flashcameleon fonctionne

--- userspace/commands.c
+++ userspace/commands.c
@@ -3,12 +3,14 @@
 #include "command_handlers.h"
 #include "log.h"
 
-void whoareyou(clientsocket_t* client, msgheader_t* header, void* body) {
+void who_are_you(clientsocket_t* client, msgheader_t* header, void* body) {
 	reset_header(header);
+
+	//TODO implement this
 
 	int fpgaVersion = 109;
 	int hpsVersion = 100;
-	char fake_mac_address[] = { 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE };
+	unsigned char fake_mac_address[] = { 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC };
 	int versionID = 300;
 
 	header->cmd = CMD_WHO_ARE_YOU;
@@ -23,7 +25,7 @@ void whoareyou(clientsocket_t* client, msgheader_t* header, void* body) {
 	int data[header->body_size];
 	data[0] = versionID;
 	data[1] = fake_mac_address[3] << 24 | fake_mac_address[2] << 16 | fake_mac_address[1] << 8 | fake_mac_address[0];
-	data[2] = fake_mac_address[4] << 8 | fake_mac_address[5];
+	data[2] = fake_mac_address[5] << 8 | fake_mac_address[4];
 	data[3] = 5; //number of hsmc slots
 	data[4] = 0;
 	data[5] = 0;
@@ -32,7 +34,52 @@ void whoareyou(clientsocket_t* client, msgheader_t* header, void* body) {
 	data[8] = 0;
 
 	if (!send_message(client, header, data)) {
-		log_error("unable to send response to WHOAREYOU");
+		log_error("unable to send response!");
+	}
+}
+
+void init_status(clientsocket_t* client, msgheader_t* header, void* body) {
+	reset_header(header);
+	header->cmd = CMD_INIT_STATUS;
+	
+	//TODO implement this
+
+	if (!send_message(client, header, NULL)) {
+		log_error("unable to send response!");
+	}
+}
+
+void read_eeprom_data(clientsocket_t* client, msgheader_t* header, void* body) {
+	reset_header(header);
+	header->cmd = CMD_READ_EEPROM_DATA;
+
+	//TODO implement this
+
+	if (!send_message(client, header, NULL)) {
+		log_error("unable to send response!");
+	}
+}
+
+void read_pio(clientsocket_t* client, msgheader_t* header, void* body) {
+	int pio_offset = header->param1;
+
+	reset_header(header);
+	header->cmd = CMD_READ_PIO;
+	header->param1 = pio_offset;
+
+	//TODO implement this
+
+	int OPTION_RXTX_GRAD_DELAY = 0x100;//bit 8 de l'ID
+	int OPTION_GRADIENT_UPDATE = 0x200;//bit 9 de l'ID
+	int OPTION_LOOP_B = 0x400;//bit 10 de l'ID
+	int ALL_OPTIONS = OPTION_LOOP_B | OPTION_GRADIENT_UPDATE | OPTION_RXTX_GRAD_DELAY;
+
+	if (pio_offset == 24) { //CameleonIO.PioOffset.SEQUENCER_ID
+		header->param2 = ALL_OPTIONS;
+	}
+
+	if (!send_message(client, header, NULL)) {
+		log_error("unable to send response!");
 	}
 }
 
@@ -40,5 +87,8 @@ void whoareyou(clientsocket_t* client, msgheader_t* header, void* body) {
 
 void register_all_commands() {
 	log_info("Registering all commands");
-	register_command_handler(CMD_WHO_ARE_YOU, whoareyou);
+	register_command_handler(CMD_WHO_ARE_YOU, who_are_you);
+	register_command_handler(CMD_INIT_STATUS, init_status);
+	register_command_handler(CMD_READ_EEPROM_DATA, read_eeprom_data);
+	register_command_handler(CMD_READ_PIO, read_pio);
 }
