f3268649262f046bb66df4bc140f29307e2e935f
Vincent Fiack <vfiack@rs2d.com>
2019-05-23 17:10:52 +0200

    fixed race condition on "size", which could be incremented and decremented by take and add at the same time.
    This provoked some interrrupts to be lost between kernel and userspace, most notablly SEQUENCE_DONE was sometimes lost, which broke the repeat-scan feature.

--- kernelspace/interrupt_queue.c
+++ kernelspace/interrupt_queue.c
@@ -6,6 +6,10 @@ typedef struct {
 	ktime_t time;
 } timed_value_t;
 
+//use a spinlock to prevent race conditions on size
+//no mutex, we're called from an interrupt handler
+static DEFINE_SPINLOCK(spinlock);
+
 //use a ring buffer
 static volatile int size = 0;
 static int next_read = 0;
@@ -27,56 +31,70 @@ bool interrupt_queue_is_empty(void) {
 }
 
 bool interrupt_queue_add(uint8_t value) {
+	unsigned long irqflags;
+	spin_lock_irqsave(&spinlock, irqflags);
+
 	if (size + 1 == INTERRUPT_QUEUE_CAPACITY) {
 		klog_warning("Unable to add item to interrupt queue, size=%d\n", size);
+		spin_unlock_irqrestore(&spinlock, irqflags);
 		return false;
 	}
 
 	timed_value_t timedvalue = { .value = value, .time = ktime_get() };
 	buffer[next_write] = timedvalue;
 	next_write = (next_write + 1) % INTERRUPT_QUEUE_CAPACITY;
-	size++;
 
+	size++;
+	spin_unlock_irqrestore(&spinlock, irqflags);
 	return true;
 }
 
-bool interrupt_queue_take(uint8_t* value) {	
+static void progressive_wait(void) {
 	if (size == 0) {
 		empty_takes++;
 
 		//select a waiting strategy depending of the queue activity.
 		if (empty_takes < 500000) {
 			//busy, return early to limit latency
-			return false;
-		} else if (empty_takes < 1000000) {
+			return;
+		}
+		else if (empty_takes < 1000000) {
 			//somewhat busy, but not that much, allow a small delay
 			usleep_range(0, 100);
-		} else {
+		}
+		else {
 			//waiting, let cpu sleep a bit
 			usleep_range(100, 1000);
 		}
+	}
+}
 
-		if (size == 0) {
-			//still empty, returns
-			return false;
-		}
+bool interrupt_queue_take(uint8_t* value) {	
+	progressive_wait();
+
+	unsigned long irqflags;
+	spin_lock_irqsave(&spinlock, irqflags);
+
+	if (size == 0) {
+		spin_unlock_irqrestore(&spinlock, irqflags);
+		return false;
 	}
 
 	timed_value_t timedvalue = buffer[next_read];
 	*value = timedvalue.value;
 	next_read = (next_read + 1) % INTERRUPT_QUEUE_CAPACITY;
+
 	size--;
-	
-	/*
+	empty_takes = 0;
+
 	ktime_t now = ktime_get();
 	ktime_t delta = ktime_sub(now, timedvalue.time);
 	u64 delta_ns = ktime_to_ns(delta);
 	if (delta_ns > 1000000) {
 		klog_warning("latency from interrupt_queue_take: size=%d / %d, time=%llu ns\n", size, INTERRUPT_QUEUE_CAPACITY, delta_ns);
 	}
-	*/
 
-	empty_takes = 0;
+	spin_unlock_irqrestore(&spinlock, irqflags);
 	return true;
 }
 
