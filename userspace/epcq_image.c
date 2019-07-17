143d2588febaed9337b95535279a0bca1359a7f1
Vincent Fiack <vfiack@rs2d.com>
2019-07-17 15:18:46 +0200

    chargement du fichier, gestion d'erreurs

--- userspace/epcq_image.c
+++ userspace/epcq_image.c
@@ -1,51 +1,79 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <linux/types.h>
+#include "std_includes.h"
 
 #define BASE_MEM 0xC8000000
 
-int32_t* map_csr_base_ptr() {
+static void fail(char* message) {
+	perror(message);
+	exit(1);
+}
+
+static int32_t* read_image_file(char* filename, int* nbytes) {
+	printf("image file: %s\n", filename);
+	FILE* f = fopen(filename, "r");
+	if (f == NULL) {
+		fail("unable to open image file");
+	}
+
+	fseek(f, 0, SEEK_END);
+	int size = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	printf("image size: %d\n", size);
+
+	void* data = malloc(size);
+	if (data == NULL) {
+		fail("malloc failed");
+	}
+
+	ssize_t total_read = 0;
+	while (total_read < size) {
+		ssize_t nread = fread(data + total_read, sizeof(char), size - total_read, f);
+		if (nread < 0) {
+			fail("Error reading file");
+		}
+
+		total_read += nread;
+	}
+	fclose(f);
+
+	*nbytes = total_read;
+	return (int32_t*)data;
+}
+
+static int32_t* map_csr_base_ptr() {
 	int fd = open("/dev/mem", O_RDWR | O_SYNC);
 
 	//EPCQ_CONTROLLER = 0xff220040, but for some reason we cannot mmap it directly
 	int span = 2000;
 	int32_t* mapped_ptr = (int32_t*)mmap(NULL, span, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0xff220000);
 	if (mapped_ptr == MAP_FAILED) {
-		perror("mmap csr failed");
-		exit(1);
+		fail("mmap csr failed");
 	}
 	
 	close(fd);
 	return mapped_ptr + 0x40;
 }
 
-int32_t* map_mem_base_ptr() {
+static int32_t* map_mem_base_ptr() {
 	int fd = open("/dev/mem", O_RDWR | O_SYNC);
 	int32_t* mapped_ptr = mmap(NULL, 0x3ffffff, PROT_WRITE, MAP_SHARED, fd, BASE_MEM);
 	if (mapped_ptr == MAP_FAILED) {
-		perror("mmap mem failed");
-		exit(1);
+		fail("mmap mem failed");
 	}
 
 	close(fd);
 	return mapped_ptr;
 }
 
-
-void wr(int32_t* csr_base_ptr, int cmd_reg, int value){
-    int* csr_ptr=csr_base_ptr+cmd_reg;
-    *csr_ptr= value;
+static void wr(int32_t* csr_base_ptr, int cmd_reg, int32_t value) {
+    *(csr_base_ptr + cmd_reg) = value;
 }
 
-void write_enable(int32_t* csr_base_ptr){
+static void write_enable(int32_t* csr_base_ptr){
 	wr(csr_base_ptr,0x7,0x00000006);
 	wr(csr_base_ptr,0x8,0x1);
 }
 
-void erase_die(int32_t* csr_base_ptr){
+static void erase_die(int32_t* csr_base_ptr){
     write_enable(csr_base_ptr);
     wr(csr_base_ptr,7,0x004C4);
     wr(csr_base_ptr,9,0x00000);
@@ -53,49 +81,40 @@ void erase_die(int32_t* csr_base_ptr){
     wr(csr_base_ptr,8,1);
 }
 
-void fastwrite_memory_block(int32_t* csr_base_ptr, int32_t* mem_base_ptr, int nb_of_bytes, int32_t* wr_data){
+static void fastwrite_memory_block(int32_t* csr_base_ptr, int32_t* mem_base_ptr, int nb_of_bytes, int32_t* data){
 	write_enable(csr_base_ptr);
 	wr(csr_base_ptr,0x1,0x00000001); 
 	wr(csr_base_ptr,0x4,0x00000220); 
 	wr(csr_base_ptr,0x0,0x00000101);
 	wr(csr_base_ptr,0x6,0x00007012);
 	for(int i=0; i<= (nb_of_bytes/4)-1; i++){
-        wr(mem_base_ptr,i,wr_data[i]);
+        wr(mem_base_ptr, i, data[i]);
         if(i%511==0)printf("done %d / %d \n",i, nb_of_bytes>>2 );
     }
 }
 
-
-void nvcr_set(int32_t* csr_base_ptr){
+static void nvcr_set(int32_t* csr_base_ptr){
     write_enable(csr_base_ptr);
     wr(csr_base_ptr,0x7,0x000002B1);
     wr(csr_base_ptr,0xA,0x0000FFFE);
     wr(csr_base_ptr,8,1);
 }
 
-
 int epcq_image_main(int argc, char** argv) {
-	int32_t* csr_base_ptr = map_csr_base_ptr();
-	int32_t* mem_base_ptr = map_mem_base_ptr();
-    int rpd_size;
+	char * filename = "output_file_auto.rpd";
+	int size = 0;
+	int32_t* data = read_image_file(filename, &size);
 
-     //open file rpd
-     FILE* frd=fopen("output_file_auto.rpd","r");
-     //check size rpd
-     fseek(frd, 0L, SEEK_END);
-     rpd_size=ftell(frd);
-     printf("file size = %d \n", rpd_size); 
-     rewind(frd); 
-        
-     int* wr_value_array=malloc(rpd_size); 
-     nvcr_set(csr_base_ptr);
-     write_enable(csr_base_ptr);
-     printf("Start ERASING FLASH DIE : wait\n"); 
-     erase_die(csr_base_ptr); //bloque le HPS pendant 1mn...
-        
-     fread(wr_value_array,sizeof(char),rpd_size,frd); 
-     fastwrite_memory_block(csr_base_ptr,mem_base_ptr,rpd_size,wr_value_array);
+	int32_t* csr_base_ptr = map_csr_base_ptr();
+	nvcr_set(csr_base_ptr);
+    write_enable(csr_base_ptr);
+    printf("Start ERASING FLASH DIE : wait\n"); 
+    erase_die(csr_base_ptr); //bloque le HPS pendant 1mn...
+
+	int32_t* mem_base_ptr = map_mem_base_ptr();
+    fastwrite_memory_block(csr_base_ptr, mem_base_ptr, size, data);
      
-	 return 0;
+	free(data);
+	return 0;
 }
     
