9d40cb20eed13563e0a9567fcb35ebc39a2d9581
Vincent Fiack <vfiack@rs2d.com>
2019-07-18 15:03:52 +0200

    intégration Jenkins

--- make-config/cross-compile.mk
+++ make-config/cross-compile.mk
@@ -1,7 +1,7 @@
 export ARCH := arm
 
 # arm-linux-gnueabihf-gcc (crosstool-NG linaro-1.13.1-4.9-2014.09 - Linaro GCC 4.9-2014.09) 4.9.2 20140904 (prerelease)
-export TOOLCHAIN_DIR := ~/BR/HOST/usr/bin/
+export TOOLCHAIN_DIR := ~hps/BR/HOST/usr/bin/
 
 # arm-linux-gnueabihf-gcc (Ubuntu/Linaro 4.6.3-1ubuntu5) 4.6.3
 #export TOOLCHAIN_DIR := /usr/bin/
