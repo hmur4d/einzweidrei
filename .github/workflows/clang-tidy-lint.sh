b680e3bd8598617ed822963c6d952d5645c91f0e
Mark Carlson <carlsonmark@gmail.com>
2020-10-16 16:33:06 -0600

    Typo: bithub -> github

--- .github/workflows/clang-tidy-lint.sh
+++ .github/workflows/clang-tidy-lint.sh
@@ -7,7 +7,9 @@ THIS_DIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 BASE_BRANCH=${1:-master}
 echo "Comparing ${BASE_BRANCH} -> $(git rev-parse --abbrev-ref HEAD)"
 
+# Directories that support clang-tidy
+DIRS=userspace
 # Use a minimal set of build flags
 BUILD_FLAGS=-std=c99
-git diff origin/${BASE_BRANCH} --diff-filter=ACMRTUXB | clang-tidy-diff -p1 -- ${BUILD_FLAGS}
+git diff origin/${BASE_BRANCH} --diff-filter=ACMRTUXB ${DIRS} | clang-tidy-diff -p1 -- ${BUILD_FLAGS}
 exit $?
