fd07c4a9fbb6c0dab8aab467f6d36b9a20d7e931
joel <joel@minski.org>
2020-12-18 16:39:26 -0700

    Fix issue in ADS126X_GatherSingle() where variables were not initialized.

--- userspace/hw_fieldlock_ads1261.c
+++ userspace/hw_fieldlock_ads1261.c
@@ -1471,6 +1471,13 @@ double ADS126X_GatherSingle(const ADS126X_INPUTS_ENUM eInput, const uint8_t bNum
 			usleep(ADS126X_GATHER_ARRAY[eInput].dwSettleUs);
 		}
 
+		// Ensure the variables are initialized
+		dbResult = 0.0;
+		if (NULL != pbStatusByte)
+		{
+			*pbStatusByte = 0x00;
+		}
+
 		for (uint32_t dwConversions=0; dwConversions<bNumConversions; dwConversions++)
 		{
 			ADS126X_StartAll();
